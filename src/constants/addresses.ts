556a3e32078b76cb1b0e6cb75f73fe28b1d119ef
lucyTrump <lucytump@dtx.trade>
2024-06-18 14:27:12 +0800

    feat: code init

---
+++ src/constants/addresses.ts
@@ -0,0 +1,139 @@
+import { FACTORY_ADDRESS as V2_FACTORY_ADDRESS } from '@uniswap/v2-sdk'
+import { FACTORY_ADDRESS as V3_FACTORY_ADDRESS } from '@uniswap/v3-sdk'
+
+import { constructSameAddressMap } from '../utils/constructSameAddressMap'
+import { SupportedChainId } from './chains'
+
+type AddressMap = { [chainId: number]: string }
+
+export const UNI_ADDRESS: AddressMap = constructSameAddressMap('0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984')
+export const MULTICALL_ADDRESS: AddressMap = {
+  ...constructSameAddressMap('0x1F98415757620B543A52E61c46B32eB19261F984', [
+    SupportedChainId.OPTIMISTIC_KOVAN,
+    SupportedChainId.OPTIMISM,
+    SupportedChainId.POLYGON_MUMBAI,
+    SupportedChainId.POLYGON,
+  ]),
+  [SupportedChainId.ARBITRUM_ONE]: '0xadF885960B47eA2CD9B55E6DAc6B42b7Cb2806dB',
+  [SupportedChainId.ARBITRUM_RINKEBY]: '0xa501c031958F579dB7676fF1CE78AD305794d579',
+  [SupportedChainId.BASE_SEPOLIA]: '0xebaBc4CFDc5B9F3eC8741A62F56ab37A68e7B1EE',
+}
+export const V2_FACTORY_ADDRESSES: AddressMap = {
+  ...constructSameAddressMap(V2_FACTORY_ADDRESS),
+  // TODO:
+  [SupportedChainId.BASE_SEPOLIA]: '0xfd8D39298478F7b88FD7687F4B470d095bb8c7Bd',
+}
+
+export const V2_ROUTER_ADDRESS: AddressMap = {
+  ...constructSameAddressMap('0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'),
+  // TODO
+  [SupportedChainId.BASE_SEPOLIA]: '0xAaF04917032161dD6BA5020241eeF79F87a85DC6',
+}
+export const V3_ROUTER_ADDRESS: AddressMap = {
+  ...constructSameAddressMap('0xE592427A0AEce92De3Edee1F18E0157C05861564', [
+    SupportedChainId.OPTIMISM,
+    SupportedChainId.OPTIMISTIC_KOVAN,
+    SupportedChainId.ARBITRUM_ONE,
+    SupportedChainId.ARBITRUM_RINKEBY,
+    SupportedChainId.POLYGON,
+    SupportedChainId.POLYGON_MUMBAI,
+  ]),
+  [SupportedChainId.BASE_SEPOLIA]: '0xF66bb719832E76F943636a2aB6e9FC432b76643D',
+}
+export const SWAP_ROUTER_ADDRESSES: AddressMap = {
+  ...constructSameAddressMap('0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45', [
+    SupportedChainId.OPTIMISM,
+    SupportedChainId.OPTIMISTIC_KOVAN,
+    SupportedChainId.ARBITRUM_ONE,
+    SupportedChainId.ARBITRUM_RINKEBY,
+    SupportedChainId.POLYGON,
+    SupportedChainId.POLYGON_MUMBAI,
+  ]),
+  [SupportedChainId.BASE_SEPOLIA]: '0xa4CF852659Dab7CD26701BA58E7d4d372cb37e0F',
+}
+
+/**
+ * The oldest V0 governance address
+ */
+export const GOVERNANCE_ALPHA_V0_ADDRESSES: AddressMap = constructSameAddressMap(
+  '0x5e4be8Bc9637f0EAA1A755019e06A68ce081D58F'
+)
+/**
+ * The older V1 governance address
+ */
+export const GOVERNANCE_ALPHA_V1_ADDRESSES: AddressMap = {
+  [SupportedChainId.MAINNET]: '0xC4e172459f1E7939D522503B81AFAaC1014CE6F6',
+}
+/**
+ * The latest governor bravo that is currently admin of timelock
+ */
+export const GOVERNANCE_BRAVO_ADDRESSES: AddressMap = {
+  [SupportedChainId.MAINNET]: '0x408ED6354d4973f66138C91495F2f2FCbd8724C3',
+}
+
+export const TIMELOCK_ADDRESS: AddressMap = constructSameAddressMap('0x1a9C8182C09F50C8318d769245beA52c32BE35BC')
+
+export const MERKLE_DISTRIBUTOR_ADDRESS: AddressMap = {
+  [SupportedChainId.MAINNET]: '0x090D4613473dEE047c3f2706764f49E0821D256e',
+}
+export const ARGENT_WALLET_DETECTOR_ADDRESS: AddressMap = {
+  [SupportedChainId.MAINNET]: '0xeca4B0bDBf7c55E9b7925919d03CbF8Dc82537E8',
+}
+export const V3_CORE_FACTORY_ADDRESSES: AddressMap = {
+  ...constructSameAddressMap(V3_FACTORY_ADDRESS, [
+    SupportedChainId.OPTIMISM,
+    SupportedChainId.OPTIMISTIC_KOVAN,
+    SupportedChainId.ARBITRUM_ONE,
+    SupportedChainId.ARBITRUM_RINKEBY,
+    SupportedChainId.POLYGON_MUMBAI,
+    SupportedChainId.POLYGON,
+  ]),
+  [SupportedChainId.BASE_SEPOLIA]: '0xb4D27C618a44cF8f7300845057A643a5Af67a89a',
+}
+export const QUOTER_ADDRESSES: AddressMap = {
+  ...constructSameAddressMap('0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6', [
+    SupportedChainId.OPTIMISM,
+    SupportedChainId.OPTIMISTIC_KOVAN,
+    SupportedChainId.ARBITRUM_ONE,
+    SupportedChainId.ARBITRUM_RINKEBY,
+    SupportedChainId.POLYGON_MUMBAI,
+    SupportedChainId.POLYGON,
+  ]),
+  [SupportedChainId.BASE_SEPOLIA]: '0x5B4b7Dd837AAF3ceeB15D4e6317455015Dad0A3E',
+}
+export const NONFUNGIBLE_POSITION_MANAGER_ADDRESSES: AddressMap = {
+  ...constructSameAddressMap('0xC36442b4a4522E871399CD717aBDD847Ab11FE88', [
+    SupportedChainId.OPTIMISM,
+    SupportedChainId.OPTIMISTIC_KOVAN,
+    SupportedChainId.ARBITRUM_ONE,
+    SupportedChainId.ARBITRUM_RINKEBY,
+    SupportedChainId.POLYGON_MUMBAI,
+    SupportedChainId.POLYGON,
+  ]),
+  [SupportedChainId.BASE_SEPOLIA]: '0x6776FbE3B437b8F2f5dfC4BaEC536CD5Be076E14',
+}
+export const ENS_REGISTRAR_ADDRESSES: AddressMap = {
+  [SupportedChainId.MAINNET]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+  [SupportedChainId.ROPSTEN]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+  [SupportedChainId.GOERLI]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+  [SupportedChainId.RINKEBY]: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+}
+export const SOCKS_CONTROLLER_ADDRESSES: AddressMap = {
+  [SupportedChainId.MAINNET]: '0x65770b5283117639760beA3F867b69b3697a91dd',
+}
+
+export const V3_MIGRATOR_ADDRESSES: AddressMap = {
+  ...constructSameAddressMap('0xA5644E29708357803b5A882D272c41cC0dF92B34', [
+    SupportedChainId.ARBITRUM_ONE,
+    SupportedChainId.ARBITRUM_RINKEBY,
+    SupportedChainId.POLYGON_MUMBAI,
+    SupportedChainId.POLYGON,
+  ]),
+  [SupportedChainId.BASE_SEPOLIA]: '0xbed424B20f6ee268E8cC6547F376A0Bd19BB0D81',
+}
+
+export const TICK_LENS_ADDRESSES: AddressMap = {
+  [SupportedChainId.ARBITRUM_ONE]: '0xbfd8137f7d1516D3ea5cA83523914859ec47F573',
+  [SupportedChainId.ARBITRUM_RINKEBY]: '0xbfd8137f7d1516D3ea5cA83523914859ec47F573',
+  [SupportedChainId.BASE_SEPOLIA]: '0x462f0b860ca7CA22D6A4F313492881EE7C5c8906',
+}
